e3628c8910e9af1bee32f0ff16d5df924aeada21
HRan2004 <2125764918@qq.com>
2021-06-09 14:46:36 +0800

    新增LOGO，主页优化，添加域名，controller层完善

--- src/main/java/com/hraps/blocklogic/BlocklogicApplication.java
+++ src/main/java/com/hraps/blocklogic/BlocklogicApplication.java
@@ -14,8 +14,15 @@ public class BlocklogicApplication implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-        WebMvcConfigurer.super.addResourceHandlers(registry);
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        // ***加入如下路径，才能正常访问到放在static文件夹下的静态资源***
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
     }
 
 }
--- src/main/java/com/hraps/blocklogic/controller/MainController.java
+++ src/main/java/com/hraps/blocklogic/controller/MainController.java
@@ -1,10 +1,14 @@
 package com.hraps.blocklogic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-
+    @RequestMapping(value = {"","/index"})
+    public String index(Model model){
+        return "index";
+    }
 }
