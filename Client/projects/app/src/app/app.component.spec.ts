6527ce5e26ae2ae49e410e0d5f56ed7934b3e3b3
Char2s <Char2s@outlook.com>
2022-03-04 15:08:55 +0800

    feat(client/app): 移除示例代码

--- Client/projects/app/src/app/app.component.spec.ts
+++ Client/projects/app/src/app/app.component.spec.ts
@@ -16,19 +16,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'app app is running!',
-    );
-  });
 });
