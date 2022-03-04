9e241b5839f51c2a4d5d8d2040bf6591af1c64e4
Char2s <Char2s@outlook.com>
2022-03-04 17:15:40 +0800

    feat(client/app/space): 添加header

--- Client/projects/app/src/app/space/space.module.ts
+++ Client/projects/app/src/app/space/space.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
+import { LayoutModule } from '../components/layout/layout.module';
 import { SharedModule } from '../shared/shared.module';
 import { SpaceComponent } from './space.component';
 import { SpaceRoutingModule } from './space-routing.module';
 
 @NgModule({
   declarations: [SpaceComponent],
-  imports: [SharedModule, SpaceRoutingModule],
+  imports: [SharedModule, SpaceRoutingModule, LayoutModule, NzLayoutModule],
 })
 export class SpaceModule {}
