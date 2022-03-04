af12735443e5d45287636a9fabe12057f0a8de93
Char2s <Char2s@outlook.com>
2022-03-04 17:12:07 +0800

    feat(client/app/components/layout/header): 实现

--- Client/projects/app/src/app/components/layout/header/header.component.ts
+++ Client/projects/app/src/app/components/layout/header/header.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.less'],
 })
 export class HeaderComponent implements OnInit {
+  links: HeaderLink[] = [
+    { text: '主页', url: '' },
+    { text: '简介', url: '' },
+    { text: '教程', url: '' },
+    { text: '学习', url: '' },
+    { text: '示例', url: '' },
+    { text: '社区', url: '' },
+    { text: '我的', url: '' },
+    { text: '联系我们', url: '' },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
+}
+
+interface HeaderLink {
+  text: string;
+  url: string;
 }
--- Client/projects/app/src/app/components/layout/layout.module.ts
+++ Client/projects/app/src/app/components/layout/layout.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 import { SharedModule } from '../../shared/shared.module';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [SharedModule],
+  imports: [SharedModule, NzLayoutModule, NzMenuModule],
   exports: [HeaderComponent],
 })
 export class LayoutModule {}
