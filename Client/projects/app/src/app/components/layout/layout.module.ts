bc33240e78af2e61bb484f183609de155072a6ca
Char2s <Char2s@outlook.com>
2022-03-04 16:05:20 +0800

    feat(client/app/components/layout/header): 初始化

--- Client/projects/app/src/app/components/layout/layout.module.ts
+++ Client/projects/app/src/app/components/layout/layout.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../../shared/shared.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [SharedModule],
+  exports: [HeaderComponent],
 })
 export class LayoutModule {}
