3f9c66eb85c26c3d2d89b75a3ff5e1ab1b32a038
Char2s <Char2s@outlook.com>
2022-03-05 16:52:13 +0800

    revert: "feat(client/app/space/toolbar): 实现基础布局"
    This reverts commit 481fbbeb80869cfc4b1ed64cd8655a4ace629f79.

--- Client/projects/app/src/app/space/space.module.ts
+++ Client/projects/app/src/app/space/space.module.ts
@@ -1,26 +1,14 @@
 import { NgModule } from '@angular/core';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 
-import { SheetModule } from '../components/sheet/sheet.module';
 import { SharedModule } from '../shared/shared.module';
 import { SpaceComponent } from './space.component';
 import { SpaceHeaderComponent } from './space-header/space-header.component';
 import { SpaceRoutingModule } from './space-routing.module';
-import { SpaceToolbarComponent } from './space-toolbar/space-toolbar.component';
 
 @NgModule({
-  declarations: [SpaceComponent, SpaceHeaderComponent, SpaceToolbarComponent],
-  imports: [
-    SharedModule,
-    SpaceRoutingModule,
-    SheetModule,
-    NzLayoutModule,
-    NzMenuModule,
-    NzButtonModule,
-    NzCardModule,
-  ],
+  declarations: [SpaceComponent, SpaceHeaderComponent],
+  imports: [SharedModule, SpaceRoutingModule, NzLayoutModule, NzMenuModule],
 })
 export class SpaceModule {}
