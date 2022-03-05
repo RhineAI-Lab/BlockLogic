481fbbeb80869cfc4b1ed64cd8655a4ace629f79
Char2s <Char2s@outlook.com>
2022-03-05 14:52:27 +0800

    feat(client/app/space/toolbar): 实现基础布局

---
+++ Client/projects/app/src/app/space/space-toolbar/space-toolbar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-space-toolbar',
+  templateUrl: './space-toolbar.component.html',
+  styleUrls: ['./space-toolbar.component.less'],
+})
+export class SpaceToolbarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- Client/projects/app/src/app/space/space.module.ts
+++ Client/projects/app/src/app/space/space.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 
+import { SheetModule } from '../components/sheet/sheet.module';
 import { SharedModule } from '../shared/shared.module';
 import { SpaceComponent } from './space.component';
 import { SpaceHeaderComponent } from './space-header/space-header.component';
 import { SpaceRoutingModule } from './space-routing.module';
+import { SpaceToolbarComponent } from './space-toolbar/space-toolbar.component';
 
 @NgModule({
-  declarations: [SpaceComponent, SpaceHeaderComponent],
-  imports: [SharedModule, SpaceRoutingModule, NzLayoutModule, NzMenuModule],
+  declarations: [SpaceComponent, SpaceHeaderComponent, SpaceToolbarComponent],
+  imports: [
+    SharedModule,
+    SpaceRoutingModule,
+    SheetModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzButtonModule,
+    NzCardModule,
+  ],
 })
 export class SpaceModule {}
