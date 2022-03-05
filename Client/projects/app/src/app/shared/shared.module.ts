a8fcc963bb83baa85e0937b9824ed5161984306f
Char2s <Char2s@outlook.com>
2022-03-05 11:41:39 +0800

    feat(client/app/shared): 添加`FlexLayoutModule`

--- Client/projects/app/src/app/shared/shared.module.ts
+++ Client/projects/app/src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  exports: [CommonModule, FormsModule],
+  exports: [CommonModule, FormsModule, FlexLayoutModule],
 })
 export class SharedModule {}
