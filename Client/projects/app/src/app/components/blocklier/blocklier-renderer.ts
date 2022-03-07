b1c6bcbe0034ab516b70b8992d52532a69765510
Char2s <Char2s@outlook.com>
2022-03-07 17:28:00 +0800

    feat(client/app/components/blocklier): 调整图形块样式

--- Client/projects/app/src/app/components/blocklier/blocklier-renderer.ts
+++ Client/projects/app/src/app/components/blocklier/blocklier-renderer.ts
@@ -12,6 +12,23 @@ export class BlocklierRenderer extends Blockly.blockRendering.Renderer {
 class BlocklierRendererConstantProvider extends Blockly.blockRendering
   .ConstantProvider {
   override CORNER_RADIUS = 4;
+
+  override makeNotch() {
+    const width = this.NOTCH_WIDTH;
+    const height = this.NOTCH_HEIGHT;
+    const innerWidth = 7;
+    const outerWidth = (width - innerWidth) / 2;
+    function makeMainPath(dir: number) {
+      return Blockly.utils.svgPaths.line([
+        Blockly.utils.svgPaths.point(dir * outerWidth, height),
+        Blockly.utils.svgPaths.point(dir * innerWidth, 0),
+        Blockly.utils.svgPaths.point(dir * outerWidth, -height),
+      ]);
+    }
+    const pathLeft = makeMainPath(1);
+    const pathRight = makeMainPath(-1);
+    return { type: this.SHAPES.NOTCH, width, height, pathLeft, pathRight };
+  }
 }
 
 Blockly.blockRendering.register(BlocklierRenderer.name, BlocklierRenderer);
