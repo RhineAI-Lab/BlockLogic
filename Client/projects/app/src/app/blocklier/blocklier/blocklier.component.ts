ae4bad3cb1290169b0532fd2f29881aca8397fd8
HRan2004 <2125764918@qq.com>
2022-03-30 16:41:57 +0800

    feat(.../space): 显示目录中Category前图标颜色

--- Client/projects/app/src/app/blocklier/blocklier/blocklier.component.ts
+++ Client/projects/app/src/app/blocklier/blocklier/blocklier.component.ts
@@ -18,6 +18,13 @@ import { BlocklierRenderer } from '../blocklier-renderer';
   styleUrls: ['./blocklier.component.less'],
 })
 export class BlocklierComponent implements OnInit, AfterViewInit {
+  readonly allBlockList: string[] = ["常用语句","循环","变量","函数","逻辑","数学","文本","列表","颜色","基于坐标操作","基于控件操作",
+    "无障碍服务","触摸模拟对象","快捷控件操作","自定义块","设备","设备信息","控制台","HTTP","对话框","全局","本地储存",
+    "文件系统","多媒体","应用","调试工具","意图","插件","设置","电源管理","界面","常用功能","摘要/加密","文本控件","按钮控件"];
+  readonly allBlockColor: string[] = ["#AAAAAA","#5AA45A","#A45A7F","#985AA4","#5A7FA4","#5A66A4","#5AA48B","#735AA4","#A4735A",
+    "#5A66A4","#2195F1","#e5af00","#e6645c","#90b01f","#888888","#3264e1","#698a8a","#0eaf9e","#339999","#9abc86","#7476c6","#cb863a",
+    "#b9993d","#d4285c","#d56331","#cc9999","#8e30be","#e5af00","#c68a16","#52a242","#3c2eaf","#AAAAAA","#88282a","#3c2eaf","#3c2eaf"];
+
   @Output() init = new EventEmitter();
   @Output() change = new EventEmitter();
   @ViewChild('container') container!: ElementRef<HTMLDivElement>;
@@ -85,7 +92,12 @@ export class BlocklierComponent implements OnInit, AfterViewInit {
       const $label = $row.querySelector<HTMLSpanElement>('.blocklyTreeLabel')!;
       const children = this.resolveToolboxCategories($host, depth + 1);
       if (children.length) $row.click(); // expand the child categories.
-      results.push({ name: $label.innerHTML, depth, $host, $row, children });
+      const name = $label.innerHTML;
+      let color: string = "#AAAAAA";
+      if(this.allBlockList.indexOf(name)){
+        color = this.allBlockColor[this.allBlockList.indexOf(name)]
+      }
+      results.push({ name: name, depth, $host, $row, children, color});
     }
     return results;
     /* eslint-enable @typescript-eslint/no-non-null-assertion */
@@ -98,4 +110,5 @@ export interface BlocklierToolboxCategory {
   $host: HTMLDivElement;
   $row: HTMLDivElement;
   children: BlocklierToolboxCategory[];
+  color: string;
 }
--- Client/projects/app/src/app/space/space-block-editor/space-block-editor.component.ts
+++ Client/projects/app/src/app/space/space-block-editor/space-block-editor.component.ts
@@ -20,4 +20,24 @@ export class SpaceBlockEditorComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  colourBlend(c1: string, c2: string = "#FFFFFF", ratio: number = 0.6): string {
+    if(!c1){
+      return "#FFFFFF00"
+    }
+    ratio = Math.max(Math.min(Number(ratio), 1), 0);
+    let r1 = parseInt(c1.substring(1, 3), 16);
+    let g1 = parseInt(c1.substring(3, 5), 16);
+    let b1 = parseInt(c1.substring(5, 7), 16);
+    let r2 = parseInt(c2.substring(1, 3), 16);
+    let g2 = parseInt(c2.substring(3, 5), 16);
+    let b2 = parseInt(c2.substring(5, 7), 16);
+    let r = Math.round(r1 * (1 - ratio) + r2 * ratio);
+    let g = Math.round(g1 * (1 - ratio) + g2 * ratio);
+    let b = Math.round(b1 * (1 - ratio) + b2 * ratio);
+    let rs = ('0' + (r || 0).toString(16)).slice(-2);
+    let gs = ('0' + (g || 0).toString(16)).slice(-2);
+    let bs = ('0' + (b || 0).toString(16)).slice(-2);
+    return '#' + rs + gs + bs;
+  }
 }
