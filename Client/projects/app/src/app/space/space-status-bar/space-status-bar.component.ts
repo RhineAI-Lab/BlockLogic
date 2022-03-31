6202d32862a75ed3b6100a3471c7e22389ff4bcb
HRan2004 <2125764918@qq.com>
2022-04-01 04:27:03 +0800

    feat(.../space): 新增底部状态栏文本

--- Client/projects/app/src/app/space/space-status-bar/space-status-bar.component.ts
+++ Client/projects/app/src/app/space/space-status-bar/space-status-bar.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpaceStatusBarComponent implements OnInit {
 
+  leftTipText: string = '编辑器初始化完成'
+  rightTipText: string = '项目打开成功'
+
   constructor() { }
 
   ngOnInit(): void {
