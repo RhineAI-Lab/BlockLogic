d37e6c5ade770599d9d3309aaac8c2d2fd755a6b
HRan2004 <2125764918@qq.com>
2022-03-31 14:59:53 +0800

    refactor(.../space): 调整项目栏位置

--- Client/projects/app/src/app/space/space-sidebar-projects/space-sidebar-projects.component.ts
+++ Client/projects/app/src/app/space/space-sidebar-projects/space-sidebar-projects.component.ts
@@ -8,5 +8,38 @@ import { Component, OnInit } from '@angular/core';
 export class SpaceSidebarProjectsComponent implements OnInit {
   constructor() {}
 
+  data = [
+    {
+      title: 'parent 1',
+      key: '100',
+      expanded: true,
+      children: [
+        {
+          title: 'parent 1-0',
+          key: '1001',
+          expanded: true,
+          children: [
+            { title: 'leaf', key: '10010', isLeaf: true },
+            { title: 'leaf', key: '10011', isLeaf: true },
+            { title: 'leaf', key: '10012', isLeaf: true }
+          ]
+        },
+        {
+          title: 'parent 1-1',
+          key: '1002',
+          children: [{ title: 'leaf', key: '10020', isLeaf: true }]
+        },
+        {
+          title: 'parent 1-2',
+          key: '1003',
+          children: [
+            { title: 'leaf', key: '10030', isLeaf: true },
+            { title: 'leaf', key: '10031', isLeaf: true }
+          ]
+        }
+      ]
+    }
+  ]
+
   ngOnInit(): void {}
 }
