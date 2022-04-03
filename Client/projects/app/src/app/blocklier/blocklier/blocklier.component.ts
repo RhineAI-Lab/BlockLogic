87792a69aff3a08155261c966cfbdb8c5f632529
Char2s <Char2s@outlook.com>
2022-04-03 13:28:27 +0800

    refactor(client/blocklier): 解耦颜色定义

--- Client/projects/app/src/app/blocklier/blocklier/blocklier.component.ts
+++ Client/projects/app/src/app/blocklier/blocklier/blocklier.component.ts
@@ -87,12 +87,33 @@ export class BlocklierComponent implements OnInit, AfterViewInit {
       const children = this.resolveToolboxCategories($host, depth + 1);
       if (children.length) $row.click(); // expand the child categories.
       const name = $label.innerHTML;
-      const color = colors[name] ?? '#AAAAAA';
+      const colorRgba = $row.style.borderLeftColor;
+      const color = colorRgba ? rgba2hex(colorRgba) : '#aaaaaa';
       results.push({ name, depth, $host, $row, children, color });
     }
     return results;
     /* eslint-enable @typescript-eslint/no-non-null-assertion */
   }
+}
+
+/**
+ *
+ * @param rgba
+ * @see https://stackoverflow.com/a/3627747/14952417
+ */
+function rgba2hex(rgba: string): string {
+  const hex = rgba
+    .match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+\.{0,1}\d*))?\)$/)
+    ?.slice(1)
+    .map((n, i) =>
+      (i === 3 ? Math.round(parseFloat(n) * 255) : parseFloat(n))
+        .toString(16)
+        .padStart(2, '0')
+        .replace('NaN', ''),
+    )
+    .join('');
+  if (!hex) throw new Error('rgba2hex failed');
+  return `#${hex}`;
 }
 
 export interface BlocklierToolboxCategory {
@@ -103,42 +124,3 @@ export interface BlocklierToolboxCategory {
   $row: HTMLDivElement;
   children: BlocklierToolboxCategory[];
 }
-
-// TODO: refactor
-const colors: Record<string, string> = {
-  ['常用语句']: '#AAAAAA',
-  ['循环']: '#5AA45A',
-  ['变量']: '#A45A7F',
-  ['函数']: '#985AA4',
-  ['逻辑']: '#5A7FA4',
-  ['数学']: '#5A66A4',
-  ['文本']: '#5AA48B',
-  ['列表']: '#735AA4',
-  ['颜色']: '#A4735A',
-  ['基于坐标操作']: '#5A66A4',
-  ['基于控件操作']: '#2195F1',
-  ['无障碍服务']: '#e5af00',
-  ['触摸模拟对象']: '#e6645c',
-  ['快捷控件操作']: '#90b01f',
-  ['自定义块']: '#888888',
-  ['设备']: '#3264e1',
-  ['设备信息']: '#698a8a',
-  ['控制台']: '#0eaf9e',
-  ['HTTP']: '#339999',
-  ['对话框']: '#9abc86',
-  ['全局']: '#7476c6',
-  ['本地储存']: '#cb863a',
-  ['文件系统']: '#b9993d',
-  ['多媒体']: '#d4285c',
-  ['应用']: '#d56331',
-  ['调试工具']: '#cc9999',
-  ['意图']: '#8e30be',
-  ['插件']: '#e5af00',
-  ['设置']: '#c68a16',
-  ['电源管理']: '#52a242',
-  ['界面']: '#3c2eaf',
-  ['常用功能']: '#AAAAAA',
-  ['摘要/加密']: '#88282a',
-  ['文本控件']: '#3c2eaf',
-  ['按钮控件']: '#3c2eaf',
-};
