a8943a8995c07119c0dbd6b5ae952380406b156e
HRan2004 <2125764918@qq.com>
2022-04-11 07:24:09 +0800

    feat(client/space): 新增OneDark样式

--- Client/projects/app/src/app/space/space-code-editor/space-code-editor.component.ts
+++ Client/projects/app/src/app/space/space-code-editor/space-code-editor.component.ts
@@ -1,22 +1,22 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, OnInit, Output} from '@angular/core';
 import * as monaco from 'monaco-editor';
 
 import { SpaceDevelopService } from '../shared/space-develop.service';
 import { SpaceState, ThemeType } from '../shared/space-state.service';
 import { SpaceToolBarButtonType } from '../space-tool-bar/space-tool-bar.component';
 import { loadWASM } from 'onigasm';
-import { Registry } from 'monaco-textmate';
-import { wireTmGrammars } from 'monaco-editor-textmate'
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-space-code-editor',
   templateUrl: './space-code-editor.component.html',
   styleUrls: ['./space-code-editor.component.less'],
 })
-export class SpaceCodeEditorComponent implements OnInit {
+export class SpaceCodeEditorComponent implements OnInit, AfterViewInit {
   editorOptions = {
-    theme: 'vs',
+    theme: 'vs-dark',
     language: 'javascript',
+    value: 'console.log("Hello World!");',
     scrollbar: {
       verticalScrollbarSize: 10,
       horizontalScrollbarSize: 10,
@@ -36,6 +36,7 @@ export class SpaceCodeEditorComponent implements OnInit {
   constructor(
     private developService: SpaceDevelopService,
     private state: SpaceState,
+    private httpClient: HttpClient,
   ) {
     state.toolbarButtonEvent$.subscribe((v) => {
       if (!state.logicMode$.getValue()) {
@@ -56,7 +57,24 @@ export class SpaceCodeEditorComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.httpClient
+      .get('assets/monaco/themes/one-dark.json', {responseType: 'text'})
+      .subscribe({
+        next:(text: string) => {
+          monaco.editor.defineTheme('one-dark', JSON.parse(text));
+        },
+        error:(err) => {
+          console.error(err);
+        },
+      });
     // this.loadTheme();
+  }
+
+  ngAfterViewInit() {
+    this.editor = monaco.editor.create(
+      document.getElementById('monaco-editor')!,
+      this.editorOptions
+    );
   }
 
   loadTheme() {
@@ -64,24 +82,26 @@ export class SpaceCodeEditorComponent implements OnInit {
       await loadWASM('assets/onigasm/onigasm.wasm');
       const grammars = new Map();
       grammars.set('javascript', 'source.js');
-      // grammars.set('css', 'source.css');
-      // grammars.set('html', 'text.html.basic');
-      // grammars.set('typescript', 'source.ts');
-      // grammars.set('scss', 'source.css.scss');
-      // grammars.set('less', 'source.css.less');
-      // grammars.set('json', 'source.json');
-      // grammars.set('python', 'source.python');
-      const registry = new Registry({
-        getGrammarDefinition: async (scopeName) => {
-          return {
-            format: 'json',
-            content: await (
-              await fetch('assets/grammars/javascript.json')
-            ).text(),
-          };
-        },
-      });
-      await wireTmGrammars(monaco, registry, grammars, this.editor)
+      grammars.set('css', 'source.css');
+      grammars.set('html', 'text.html.basic');
+      grammars.set('typescript', 'source.ts');
+      grammars.set('scss', 'source.css.scss');
+      grammars.set('less', 'source.css.less');
+      grammars.set('json', 'source.json');
+      grammars.set('python', 'source.python');
+
+      // TODO: 取消注释下方代码会产生webpack报错
+      // const registry = new Registry({
+      //   getGrammarDefinition: async (scopeName) => {
+      //     return {
+      //       format: 'json',
+      //       content: await (
+      //         await fetch('assets/grammars/javascript.json')
+      //       ).text(),
+      //     };
+      //   },
+      // });
+      // await wireTmGrammars(monaco, registry, grammars, this.editor)
     };
     init();
   }
