7e78e3b3752b87eb8c5523e510e3858ceb6bdc4d
HRan2004 <2125764918@qq.com>
2022-04-24 23:48:34 +0800

    fix(space): 修复部分资源PWA无法加载

--- Client/projects/app/src/app/space/space.module.ts
+++ Client/projects/app/src/app/space/space.module.ts
@@ -177,7 +177,7 @@ const monacoConfig: NgxMonacoEditorConfig = {
 export class SpaceModule {
   constructor(iconService: NzIconService) {
     iconService.fetchFromIconfont({
-      scriptUrl: '/assets/iconfont.js',
+      scriptUrl: 'https://at.alicdn.com/t/font_3294553_ml83t1vx2b.js',
     });
   }
 }
