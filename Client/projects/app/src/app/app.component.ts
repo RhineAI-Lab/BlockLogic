daf922ffe81cd4e9e5f1439ebc0e8d24a42156da
HRan2004 <2125764918@qq.com>
2022-04-27 00:06:29 +0800

    feat(client): 优化地址显示

--- Client/projects/app/src/app/app.component.ts
+++ Client/projects/app/src/app/app.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {merge} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
 })
-export class AppComponent {}
+export class AppComponent{
+  constructor(
+    private router:Router,
+    private routerInfo: ActivatedRoute
+  ) {
+    merge(this.routerInfo.queryParams, this.routerInfo.fragment).subscribe(() => {
+      console.log("route change")
+    });
+  }
+}
+
