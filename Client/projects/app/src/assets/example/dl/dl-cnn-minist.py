15927adf4cf11434c54752e1c3ddbe0e1ba9b32a
HRan2004 <2125764918@qq.com>
2022-04-29 22:07:01 +0800

    feat(example): 添加cnn网络示例

---
+++ Client/projects/app/src/assets/example/dl/dl-cnn-minist.py
@@ -0,0 +1,97 @@
+import time
+import torch
+import torchvision
+from torch import nn
+
+import sys
+sys.path.append("..")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+batch_size_train = 16
+batch_size_test = 8
+train_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('I:/data/', train=True, download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),
+                                   torchvision.transforms.Normalize(
+                                       (0.1307,), (0.3081,))
+                               ])),
+    batch_size=batch_size_train, shuffle=True)
+test_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('I:/data/', train=False, download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),
+                                   torchvision.transforms.Normalize(
+                                       (0.1307,), (0.3081,))
+                               ])),
+    batch_size=batch_size_test, shuffle=True)
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 6, 5), # in_channels, out_channels, kernel_size
+            nn.Sigmoid(),
+            nn.MaxPool2d(2, 2), # kernel_size, stride
+            nn.Conv2d(6, 16, 5),
+            nn.Sigmoid(),
+            nn.MaxPool2d(2, 2)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(16*4*4, 120),
+            nn.Sigmoid(),
+            nn.Linear(120, 84),
+            nn.Sigmoid(),
+            nn.Linear(84, 10)
+        )
+
+    def forward(self, img):
+        feature = self.conv(img)
+        output = self.fc(feature.view(img.shape[0], -1))
+        return output
+
+net = LeNet()
+print(net)
+
+loss = torch.nn.CrossEntropyLoss()
+
+lr, num_epochs = 0.001, 5
+optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+
+def evaluate_accuracy(data_iter, net, device=None):
+    if device is None and isinstance(net, nn.Module):
+        device = list(net.parameters())[0].device
+    acc_sum, n = 0.0, 0
+    with torch.no_grad():
+        for X, y in data_iter:
+            if isinstance(net, nn.Module):
+                net.eval()
+                acc_sum += (net(X.to(device)).argmax(dim=1) == y.to(device)).float().sum().cpu().item()
+                net.train()
+            else:
+                if('is_training' in net.__code__.co_varnames):
+                    acc_sum += (net(X, is_training=False).argmax(dim=1) == y).float().sum().item()
+                else:
+                    acc_sum += (net(X).argmax(dim=1) == y).float().sum().item()
+            n += y.shape[0]
+    return acc_sum / n
+
+print("Training on: ", device)
+net = net.to(device)
+for epoch in range(num_epochs):
+    train_l_sum, train_acc_sum, n, batch_count, start = 0.0, 0.0, 0, 0, time.time()
+    for X, y in train_loader:
+        X, y = X.to(device), y.to(device)
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        optimizer.zero_grad()
+        l.backward()
+        optimizer.step()
+        train_l_sum += l.cpu().item()
+        train_acc_sum += (y_hat.argmax(dim=1) == y).sum().cpu().item()
+        n += y.shape[0]
+        batch_count += 1
+    test_acc = evaluate_accuracy(test_loader, net)
+    print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, time %.1f sec'
+          % (epoch + 1, train_l_sum / batch_count, train_acc_sum / n, test_acc, time.time() - start))
+
