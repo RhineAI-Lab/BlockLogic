751d17fd861aeabf587b54f934c16b9da6b99e70
Artemis.Null <dongfengyao@yeah.net>
2022-05-04 23:22:47 +0800

    feat(block): 新增转化为张量图形块

--- Client/projects/app/src/app/blocklier/definitions/array_calculation/blocks/transforms.ts
+++ Client/projects/app/src/app/blocklier/definitions/array_calculation/blocks/transforms.ts
@@ -17,5 +17,17 @@ Blockly.defineBlocksWithJsonArray([
         tooltip: '结合不同的变换',
         helpUrl: baseHelpUrl + ''
     },
-
+    {
+        type:'transforms_totensor',
+        message0: '将 %1 转换为张量',
+        args0: [
+            {type: 'input_value', name: 'IMAGE', check: 'Array'}
+        ],
+        inputsInline: true,
+        previousStatement: null,
+        nextStatement: null,
+        style: style,
+        tooltip: '将图像或者ndarray转换为张量',
+        helpUrl: baseHelpUrl + ''
+    }
 ])
--- Client/projects/app/src/app/blocklier/definitions/array_calculation/generators/transforms.ts
+++ Client/projects/app/src/app/blocklier/definitions/array_calculation/generators/transforms.ts
@@ -6,3 +6,10 @@ Python['transforms_compose'] = function (block: any) {
     const code = `transforms.Compose(${list})`;
     return code + '\n' ;
 }
+
+Python['transforms_totensor'] = function (block: any) {
+    const image = Python.valueToCode(block, 'IMAGE', Python.ORDER_ATOMIC, true) || 'None';
+    Python.provideFunction_('import_transforms', ['import transforms']);
+    const code = `transforms.ToTensor()(${image})`;
+    return code + '\n' ;
+}
