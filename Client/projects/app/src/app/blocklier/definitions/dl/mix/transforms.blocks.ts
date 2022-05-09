41643fd9e0c998df1c173160960c55b431cece10
Artemis.Null <dongfengyao@yeah.net>
2022-05-09 21:34:49 +0800

    feat(block): 新增中心裁剪和随机缩放裁剪图形块

--- Client/projects/app/src/app/blocklier/definitions/dl/mix/transforms.blocks.ts
+++ Client/projects/app/src/app/blocklier/definitions/dl/mix/transforms.blocks.ts
@@ -13,4 +13,26 @@ Python
 import: from torchvision import transforms
 transforms.RandomCrop(size=$A0,padding=$A1)
 
-`, true);
+CenterCrop: Transforms
+中心裁剪 尺寸{num,Array}
+inline: 1
+tip: 对图片进行随机裁剪
+Python
+import: from torchvision import transforms
+transforms.CenterCrop(size=$A0,padding=$A1)
+
+prefix: transforms
+style: transforms_blocks
+
+RandomResizedCrop: Transforms
+随机缩放裁剪 尺寸{num,Array} 区间从{num}到{num}
+inline: 1
+tip: 对图片进行随机裁剪
+Python
+import: from torchvision import transforms
+transforms.RandomResizedCrop(size=$A0,scale=($A1，$A2))
+`);
+
+defineBlocksWithDt(`
+
+`,true);
