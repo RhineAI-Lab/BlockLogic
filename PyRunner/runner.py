38b3e096b04cebc351d8278ab3cf44dfe2f5cc22
HRan2004 <2125764918@qq.com>
2022-05-19 04:05:21 +0800

    feat(PyRunner): 支持子进程运行Python

--- PyRunner/runner.py
+++ PyRunner/runner.py
@@ -1,7 +1,20 @@
+# -*- coding:utf-8 -*-
+import subprocess
+import time
 
 
 def run():
-    pass;
+    file = 'temp.py'
+    cmd = 'python ' + file
+    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    while popen.poll() is None:
+        line = popen.stdout.readline()
+        print(line)
+        # print(str(line,'UTF-8'))
+        time.sleep(0.1)
+
+run()
+
 
 
 
---
+++ PyRunner/temp.py
@@ -0,0 +1,6 @@
+import time
+
+for i in range(3):
+    print("Test你好")
+    time.sleep(0.5)
+
