09b82482020c20a7f0f6cc563b266ff182544edc
HRan2004 <2125764918@qq.com>
2022-05-19 08:11:09 +0800

    feat(space): 调整终端字体样式

--- Client/projects/app/src/app/space/space-sidebar-terminal/space-sidebar-terminal.component.ts
+++ Client/projects/app/src/app/space/space-sidebar-terminal/space-sidebar-terminal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import {FunctionsUsingCSI, NgTerminal} from 'ng-terminal';
+import { FunctionsUsingCSI, NgTerminal } from 'ng-terminal';
+import {ITerminalOptions} from "xterm";
 
 @Component({
   selector: 'app-space-sidebar-terminal',
@@ -8,20 +9,28 @@ import {FunctionsUsingCSI, NgTerminal} from 'ng-terminal';
 })
 export class SpaceSidebarTerminalComponent implements OnInit, AfterViewInit {
   @ViewChild('terminal', { static: true }) terminal!: NgTerminal;
+  options: ITerminalOptions = {};
 
   constructor() {}
 
   ngOnInit() {}
 
   ngAfterViewInit(): void {
-    this.terminal.keyEventInput.subscribe(e => {
-      console.log('keyboard event:' + e.domEvent.keyCode + ', ' + e.key);
+    this.options = this.terminal.underlying.options;
+    this.options.fontSize = 12;
+    this.options.letterSpacing = 1;
+
+    this.writeln('终端初始化完成');
+    this.terminal.write('$ ');
+    this.terminal.keyEventInput.subscribe((e) => {
+      // console.log('keyboard event:' + e.domEvent.keyCode + ', ' + e.key);
 
       const ev = e.domEvent;
       const printable = !ev.altKey && !ev.ctrlKey && !ev.metaKey;
 
       if (ev.keyCode === 13) {
-        this.terminal.write('\n' + FunctionsUsingCSI.cursorColumn(1) + '$ '); // \r\n
+        this.writerNextLine();
+        this.writePrefix();
       } else if (ev.keyCode === 8) {
         if (this.terminal.underlying.buffer.active.cursorX > 2) {
           this.terminal.write('\b \b');
@@ -29,6 +38,18 @@ export class SpaceSidebarTerminalComponent implements OnInit, AfterViewInit {
       } else if (printable) {
         this.terminal.write(e.key);
       }
-    })
+    });
+  }
+
+  writeln(msg: string): void {
+    this.terminal.write(msg);
+    this.writerNextLine();
+  }
+
+  writePrefix(): void {
+    this.terminal.write('$ ');
+  }
+  writerNextLine(): void {
+    this.terminal.write('\n' + FunctionsUsingCSI.cursorColumn(1)); // \r\n
   }
 }
